a6f554d76dc7c4c904c851890727ceebfc3624a0
iamebenezer <abegundeebenezer2004@gmail.com>
2023-09-29 13:16:03 +0100

    bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
 import React from 'react'
 import { Route, Routes } from "react-router-dom"
-import Home from '../pages/Home'
-import Host from "../pages/Host";
-import Register from "../pages/Register";
+import Home from './pages/Home'
+import { useState } from 'react';
+import Host from "./pages/Host";
+import Register from "./pages/Register";
 import '../src/index.css'
-function App() {
-  return (
 
+function App() {
+  const [cursorX, setCursorX] = useState()
+  const [cursorY, setCursorY] = useState()
+
+  window.addEventListener('mousemove', (e) =>{
+    setCursorX(e.pageX)
+    setCursorY(e.pageY)
+  })
+  return (
+    <>
+     <div className="cursor"
+        style={{
+          left:cursorX + 'px',
+          top:cursorY + 'px'
+        }}
+      ></div>
     <Routes>
-       <Route path="/" element={<Home/>}/>
+      
+       <Route path="/" exact element={<Home/>}/>
       <Route path="/register" element={<Register/>}/>
       <Route path="/host" element={<Host/>}/>
       
     </Routes>
-   
+   </>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
-import HeroImage from '../src/assets/banner.jpg'
+import HeroImage from '../assets/banner.jpg'
 import { Link } from 'react-router-dom'
-import '../src/index.css'
+import '../index.css'
 import { motion } from 'framer-motion';
 import { useState } from 'react';
 function Home() {
@@ -17,6 +17,7 @@ function Home() {
      animate = {{opacity:1}}
      exit={{opacity:0}}
      transition={{duration: 1}}
+     style={{maxWidth: "100vw"}}
     >
     <div>
       <div className="cursor"
@@ -31,7 +32,7 @@ function Home() {
       
        
        <div className="flex flex-col justify-center items-center">
-       <motion.h1
+       <motion.div
       initial={{y:-1000}}
       animate={{y:0}}
       transition={{
@@ -39,25 +40,31 @@ function Home() {
         delay: "0.5"
 
       }}
-      
-    >
-         <img className='mb-8 object-fit w-96 h-62 md:w-[500px]  md:h-[500px] shadow-md shadow-purple-100/30' src={HeroImage} alt="" srcset="" />
-      </motion.h1>
+      style={{
+        backgroundImage: `url(${HeroImage})`,
+        height: "100vh",
+        width: "100vw",
+        backgroundSize: "100% 100%",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center"
+       }}
+       className='flex items-end justify-center'
 
-         </div>
-         
-         
-         <div className=" space-x-3 md:flex md:space-x-10 md:mt-3">
+    >
+         {/* <img className='mb-8 object-fit w-96 h-62 md:w-[500px]  md:h-[500px] shadow-md shadow-purple-100/30' src={} alt="" srcset="" /> */}
+         <div className="space-x-3 md:flex md:space-x-10 md:mt-3 mb-14">
        
-         <Link to="/register" className='text-white bg-gradient-to-r from-purple-800 to-purple-400 px-6 py-2 rounded-lg shadow-sm font-race '>Guest</Link>
+       <Link to="/register" className='text-white bg-gradient-to-r from-purple-800 to-purple-400 px-6 py-2 rounded-lg shadow-sm font-race '>Guest</Link>
+  
+  
+  <Link to="/host" className='backdrop-contrast-200 text-white bg-gradient-to-r from-purple-800 to-purple-400 px-6 py-2 rounded-lg shadow-sm font-race '>Host</Link>
+
     
-    
-    <Link to="/host" className='text-white bg-gradient-to-r from-purple-800 to-purple-400 px-6 py-2 rounded-lg shadow-sm font-race '>Host</Link>
- 
-      
-       
-       </div>
-       
+     
+     </div>
+      </motion.div>
+
+        </div>
        
        {/* <div className="flex justify-between flex-row px-2 md:hidden">
     
