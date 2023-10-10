3510910b55651dc8d3c47c0056fa1b8fe7f531f9
David Aremu <segun8427@gmail.com>
2023-10-10 16:20:07 +0100

    Added filter implementation

--- src/pages/Participants.jsx
+++ src/pages/Participants.jsx
@@ -1,23 +1,30 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import ApiService from "../services/ApiService";
-import { GroupTypes, ParticipantTypes, getGroupType, getParticipantType } from "../utils/Types";
+import {
+  GroupTypes,
+  ParticipantTypes,
+  getGroupType,
+  getParticipantType,
+} from "../utils/Types";
 
 const Participants = () => {
   const [participants, setParticipants] = React.useState([]);
   const [churches, setChurches] = React.useState([]);
-  const [pagination, setPagination] = React.useState({links: {}, meta: {}});
+  const [pagination, setPagination] = React.useState({ links: {}, meta: {} });
+  const [type, setType] = React.useState(null);
+  const [group, setGroup] = React.useState(null);
 
   const getParticipants = async (url) => {
     try {
       const response = await ApiService.ParticipantsList(url);
       console.log(response);
-      setPagination({links: response.links, meta: response.meta});
+      setPagination({ links: response.links, meta: response.meta });
       setParticipants(response.data);
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   const getChurches = async () => {
     try {
@@ -27,11 +34,18 @@ const Participants = () => {
     } catch (error) {
       console.error(error);
     }
-  }
+  };
+
+  const filter = async () => {
+    await getParticipants(
+      `https://api.csmcgethsemanehq.com/api/youthweek/participants/${type}/${group}`
+    );
+  };
+
   React.useState(() => {
     (async () => {
-     await getParticipants();
-     await getChurches();
+      await getParticipants();
+      await getChurches();
     })();
   }, []);
   return (
@@ -41,60 +55,75 @@ const Participants = () => {
         <div className="mb-12">
           <p></p>
           <div className="grid grid-cols-1 sm:grid-cols-3 gap-1">
-          <div className="sm:col-span-1">
-              <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
+            <div className="sm:col-span-1">
+              <label
+                htmlFor="country"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
                 Type
               </label>
               <div className="mt-2">
                 <select
-
+                  value={type}
+                  onChange={(e) => setType(e.target.value)}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
                 >
-                  <option></option>
-                  {ParticipantTypes.map(type => (<option key={type.key} value={type.key}>
-                    {type.name}
-                  </option>))}
+                  <option value={null}>All</option>
+                  {ParticipantTypes.map((type) => (
+                    <option key={type.key} value={type.key}>
+                      {type.name}
+                    </option>
+                  ))}
                 </select>
               </div>
             </div>
-          <div className="sm:col-span-1">
-              <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
+            <div className="sm:col-span-1">
+              <label
+                htmlFor="country"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
                 Group
               </label>
               <div className="mt-2">
                 <select
-
+                  value={group}
+                  onChange={(e) => setGroup(e.target.value)}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
                 >
-                  <option></option>
-                  {GroupTypes.map(type => (<option key={type.key} value={type.key}>
-                    {type.name}
-                  </option>))}
+                  <option value={null}>All</option>
+                  {GroupTypes.map((type) => (
+                    <option key={type.key} value={type.key}>
+                      {type.name}
+                    </option>
+                  ))}
                 </select>
               </div>
             </div>
-          <div className="sm:col-span-1">
-              <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
+            <div className="sm:col-span-1">
+              <label
+                htmlFor="country"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
                 Churches
               </label>
               <div className="mt-2">
-                <select
-
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                >
-                  <option></option>
-                  {churches.map(church => (<option key={church.id} value={church.id}>
-                    {church.name}
-                  </option>))}
+                <select className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
+                  <option>All</option>
+                  {churches.map((church) => (
+                    <option key={church.id} value={church.id}>
+                      {church.name}
+                    </option>
+                  ))}
                 </select>
               </div>
             </div>
           </div>
           <button
-          className=" mt-4 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          Search
-        </button>
+            onClick={() => filter()}
+            className=" mt-4 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Search
+          </button>
         </div>
         <div className="overflow-x-auto rounded-t-lg">
           <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
@@ -148,18 +177,18 @@ const Participants = () => {
           <ol className="flex justify-end gap-1 text-xs font-medium">
             <li>
               <button
-                 disabled={pagination.links.first ? false : true }
-                  onClick={e => getParticipants(pagination.links.first)}
+                disabled={pagination.links.first ? false : true}
+                onClick={(e) => getParticipants(pagination.links.first)}
                 className="inline-flex h-8 w-8 items-center justify-center rounded border border-gray-100 bg-white text-gray-900 rtl:rotate-180"
               >
                 <span className="sr-only">First Page</span>
-               First
+                First
               </button>
             </li>
             <li>
               <button
-              disabled={pagination.links.prev ? false : true }
-                  onClick={e => getParticipants(pagination.links.prev)}
+                disabled={pagination.links.prev ? false : true}
+                onClick={(e) => getParticipants(pagination.links.prev)}
                 className="inline-flex h-8 w-8 items-center justify-center rounded border border-gray-100 bg-white text-gray-900 rtl:rotate-180"
               >
                 <span className="sr-only">Prev Page</span>
@@ -178,12 +207,10 @@ const Participants = () => {
               </button>
             </li>
 
-           
-
             <li>
               <button
-               disabled={pagination.links.next ? false : true }
-               onClick={e => getParticipants(pagination.links.next)}
+                disabled={pagination.links.next ? false : true}
+                onClick={(e) => getParticipants(pagination.links.next)}
                 className="inline-flex h-8 w-8 items-center justify-center rounded border border-gray-100 bg-white text-gray-900 rtl:rotate-180"
               >
                 <span className="sr-only">Next Page</span>
@@ -203,12 +230,12 @@ const Participants = () => {
             </li>
             <li>
               <button
-                 disabled={pagination.links.last ? false : true }
-                  onClick={e => getParticipants(pagination.links.last)}
+                disabled={pagination.links.last ? false : true}
+                onClick={(e) => getParticipants(pagination.links.last)}
                 className="inline-flex h-8 w-8 items-center justify-center rounded border border-gray-100 bg-white text-gray-900 rtl:rotate-180"
               >
                 <span className="sr-only">Last Page</span>
-               Last
+                Last
               </button>
             </li>
           </ol>
